7c4822be2e1753ecd4d063faba4391975eda8a42
Hubert Lee <hubert.k.lee@gmail.com>
2014-06-21 13:23:30 -0400

    enable js

--- WebViewJsTest/src/main/java/com/scubedsoft/webviewjs/MainActivity.java
+++ WebViewJsTest/src/main/java/com/scubedsoft/webviewjs/MainActivity.java
@@ -3,6 +3,7 @@ package com.scubedsoft.webviewjs;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebSettings;
 
 public class MainActivity extends Activity
 {
@@ -17,6 +18,8 @@ public class MainActivity extends Activity
     public void onResume() {
         super.onResume();
         WebView wv = (WebView)findViewById(R.id.main);
+        WebSettings wvs = wv.getSettings();
+        wvs.setJavaScriptEnabled(true);
         wv.loadUrl("http://hube.github.io/WebViewJsTest/test.html");
     }
 }
