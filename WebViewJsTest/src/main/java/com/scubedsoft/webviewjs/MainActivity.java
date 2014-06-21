6f75918f9348a09a2d58deee2b935a4e99d0b385
Hubert Lee <hubert.k.lee@gmail.com>
2014-06-21 13:04:00 -0400

    use WebView to load page from url

--- WebViewJsTest/src/main/java/com/scubedsoft/webviewjs/MainActivity.java
+++ WebViewJsTest/src/main/java/com/scubedsoft/webviewjs/MainActivity.java
@@ -2,14 +2,21 @@ package com.scubedsoft.webviewjs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        WebView wv = (WebView)findViewById(R.id.main);
+        wv.loadUrl("http://hube.github.io/WebViewJsTest/test.html");
     }
 }
